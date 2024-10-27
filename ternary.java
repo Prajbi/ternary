04b29095c7400ad8557133c2a7533690b7f20214
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-10-27 17:36:55 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,17 @@
+public class ternary {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        // int result;
+        // if (a % 2 == 0) {
+        // result = 10;
+        // } else {
+        // result = 20;
+        // }
+        // result = a % 2 == 0 ? 10 : 20;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+
+    }
+}
